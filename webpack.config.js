65a766ad3bee915fcb6ae00367fe99ca0059ec2e
Tsimafei Kalinouski <tsimafei_kalinouski@epam.com>
2023-04-27 23:48:01 +0300

    feat: resolve task 1

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'production',
-  entry: './src/task1.js',
+  entry: './src/task2.js',
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
